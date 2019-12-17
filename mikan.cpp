0a81514aa2bd7253c7f77dd93cb15027cc0f5199
ngtrr <sryohei2429@gmail.com>
2019-12-17 17:04:51 +0900

    maybe the problem is in elasticity

--- mikan.cpp
+++ mikan.cpp
@@ -48,14 +48,14 @@ using namespace Eigen;
 	double Ms = 6.02E+5;
   	double K1 = 2.7E+3, K2 = -6.1E+3;
   	double ram100 = 0.0E+4, ram111 = 1.64E-3;
-  	double c11 = 1.495E+11, c12 = 1.43E+11, c44 = 7.05E+10;
+  	double c11 = 1.495E+11, c12 = 1.43E+12, c44 = 7.05E+10;
 	double A = 2.0E-11;
   	double Astar;
 	double delt = 0.01;
 	double mu0 = 1.0;
 	double ld = 1.8E-8;
 
-	double G = 1.0E-15/(250*ld*ld);
+	double G = 1.0E-4/(250*ld*ld);
 
 	double Q1 = 2.32E+10;
 	double Q2 = -0.63E+10;
@@ -69,7 +69,7 @@ using namespace Eigen;
 	double B = 4.00E+6;
 
 
-	double smob = 3.0E-11;
+	double smob = 1.0E-11;
 	double ds_fac = 0.001;
 
 	double root3 = 1.73205080757;
@@ -379,12 +379,12 @@ start: ;
 	}
 
 
-	epsilon_homo[0][0] = s[0][0][0][0] * sigma_a[0][0] + s[0][0][1][1] * (sigma_a[1][1] + sigma_a[2][2]) + epsilon_sum[0][0]/SIZE;
-	epsilon_homo[1][1] = s[0][0][0][0] * sigma_a[1][1] + s[0][0][1][1] * (sigma_a[0][0] + sigma_a[2][2]) + epsilon_sum[1][1]/SIZE;
-	epsilon_homo[2][2] = s[0][0][0][0] * sigma_a[2][2] + s[0][0][1][1] * (sigma_a[0][0] + sigma_a[1][1]) + epsilon_sum[2][2]/SIZE;
-	epsilon_homo[0][1] = epsilon_homo[1][0] = 1/2 * s[0][1][0][1] * sigma_a[0][1] + epsilon_sum[0][1]/SIZE;
-	epsilon_homo[1][2] = epsilon_homo[2][1] = 1/2 * s[0][1][0][1] * sigma_a[1][2] + epsilon_sum[1][2]/SIZE;
-	epsilon_homo[2][0] = epsilon_homo[0][2] = 1/2 * s[0][1][0][1] * sigma_a[2][0] + epsilon_sum[2][0]/SIZE;
+	//epsilon_homo[0][0] = s[0][0][0][0] * sigma_a[0][0] + s[0][0][1][1] * (sigma_a[1][1] + sigma_a[2][2]) + epsilon_sum[0][0]/SIZE;
+	//epsilon_homo[1][1] = s[0][0][0][0] * sigma_a[1][1] + s[0][0][1][1] * (sigma_a[0][0] + sigma_a[2][2]) + epsilon_sum[1][1]/SIZE;
+	//epsilon_homo[2][2] = s[0][0][0][0] * sigma_a[2][2] + s[0][0][1][1] * (sigma_a[0][0] + sigma_a[1][1]) + epsilon_sum[2][2]/SIZE;
+	//epsilon_homo[0][1] = epsilon_homo[1][0] = 1/2 * s[0][1][0][1] * sigma_a[0][1] + epsilon_sum[0][1]/SIZE;
+	//epsilon_homo[1][2] = epsilon_homo[2][1] = 1/2 * s[0][1][0][1] * sigma_a[1][2] + epsilon_sum[1][2]/SIZE;
+	//epsilon_homo[2][0] = epsilon_homo[0][2] = 1/2 * s[0][1][0][1] * sigma_a[2][0] + epsilon_sum[2][0]/SIZE;
 
 	for(l=0;l<3;l++){
 		for(k=0;k<3;k++){
@@ -508,7 +508,7 @@ start: ;
 	for(i=0;i<=ndm;i++){
 		for(j=0;j<=ndm;j++){
 			for(k=0;k<3;k++){
-				P[i][j][k] = Plandau[i][j][k] + Pme[i][j][k];
+				P[i][j][k] = Plandau[i][j][k] + Pelastic[i][j][k] + Pme[i][j][k];
 			}
 		}
 	}
@@ -549,8 +549,8 @@ start: ;
 		for(k=0;k<3;k++){
 			for(i=0;i<=ndm;i++){
 				for(j=0;j<=ndm;j++){
-					epsilon_zero_postfour[i][j][k][k] = (epsilon_zerofour[i][j][k][k] + -1*smob*delt*Pfour[i][j][k])/(1 + G*(four_axis(0,i,j)*four_axis(0,i,j) + four_axis(1,i,j)*four_axis(1,i,j) + four_axis(2,i,j)*four_axis(2,i,j))*1*smob*delt);
-					epsilon_zero_postfour_i[i][j][k][k] = (epsilon_zerofour_i[i][j][k][k] + -1*smob*delt*Pfour_i[i][j][k])/(1 + G*(four_axis(0,i,j)*four_axis(0,i,j) + four_axis(1,i,j)*four_axis(1,i,j) + four_axis(2,i,j)*four_axis(2,i,j))*1*smob*delt);
+					epsilon_zero_postfour[i][j][k][k] = (epsilon_zerofour[i][j][k][k] + -1*smob*delt*Pfour[i][j][k])/(1 + G*(four_axis(0,i,j)*four_axis(0,i,j) + four_axis(1,i,j)*four_axis(1,i,j) + four_axis(2,i,j)*four_axis(2,i,j))*smob*delt);
+					epsilon_zero_postfour_i[i][j][k][k] = (epsilon_zerofour_i[i][j][k][k] + -1*smob*delt*Pfour_i[i][j][k])/(1 + G*(four_axis(0,i,j)*four_axis(0,i,j) + four_axis(1,i,j)*four_axis(1,i,j) + four_axis(2,i,j)*four_axis(2,i,j))*smob*delt);
 				}
 			}
 		}
@@ -606,8 +606,8 @@ start: ;
 		for(k=0;k<3;k++){
 			for(i=0;i<=ndm;i++){
 				for(j=0;j<=ndm;j++){
-					epsilon_zero_postfour[i][j][k][k] = ((4*epsilon_zero_prefour[i][j][k][k] - epsilon_zerofour[i][j][k][k]) + 2*-1*smob*delt*(2*Pfour[i][j][k] - P_prefour[i][j][k]))/(3 + 2*G*(four_axis(0,i,j)*four_axis(0,i,j) + four_axis(1,i,j)*four_axis(1,i,j) + four_axis(2,i,j)*four_axis(2,i,j))*1*smob*delt);
-					epsilon_zero_postfour_i[i][j][k][k] = ((4*epsilon_zero_prefour_i[i][j][k][k] - epsilon_zerofour_i[i][j][k][k]) + 2*-1*smob*delt*(2*Pfour_i[i][j][k] - P_prefour_i[i][j][k]))/(3 + 2*G*(four_axis(0,i,j)*four_axis(0,i,j) + four_axis(1,i,j)*four_axis(1,i,j) + four_axis(2,i,j)*four_axis(2,i,j))*1*smob*delt);
+					epsilon_zero_postfour[i][j][k][k] = ((4*epsilon_zero_prefour[i][j][k][k] - epsilon_zerofour[i][j][k][k]) + 2*-1*smob*delt*(2*Pfour[i][j][k] - P_prefour[i][j][k]))/(3 + 2*G*(four_axis(0,i,j)*four_axis(0,i,j) + four_axis(1,i,j)*four_axis(1,i,j) + four_axis(2,i,j)*four_axis(2,i,j))*smob*delt);
+					epsilon_zero_postfour_i[i][j][k][k] = ((4*epsilon_zero_prefour_i[i][j][k][k] - epsilon_zerofour_i[i][j][k][k]) + 2*-1*smob*delt*(2*Pfour_i[i][j][k] - P_prefour_i[i][j][k]))/(3 + 2*G*(four_axis(0,i,j)*four_axis(0,i,j) + four_axis(1,i,j)*four_axis(1,i,j) + four_axis(2,i,j)*four_axis(2,i,j))*smob*delt);
 				}
 			}
 		}
@@ -1014,9 +1014,9 @@ void graph_s1()
 			col_R=epsilon_zero[i][j][0][0];//場の色をRGBにて設定
 			col_G=epsilon_zero[i][j][1][1];
 			col_B=epsilon_zero[i][j][2][2];
-			col_R *= 500;
-			col_G *= 500;
-			col_B *= 500;
+			col_R *= 10000;
+			col_G *= 10000;
+			col_B *= 10000;
 			col_R += 128;
 			col_G += 128;
 			col_B += 128;
@@ -1025,6 +1025,23 @@ void graph_s1()
 		}
 	}
 	cv::imwrite("LLG_Terfenol_" + std::to_string(int(time1)) + "_strain_2d.png", chann);
+
+	for(i=0;i<=ndm;i++){
+		for(j=0;j<=ndm;j++){
+			col_R=u[i][j][0];//場の色をRGBにて設定
+			col_G=u[i][j][1];
+			col_B=u[i][j][2];
+			col_R *= 100;
+			col_G *= 100;
+			col_B *= 100;
+			col_R += 128;
+			col_G += 128;
+			col_B += 128;
+
+			chann.at<cv::Vec3b>(i,j) = cv::Vec3b(abs(int(col_B)), abs(int(col_G)), abs(int(col_R)));
+		}
+	}
+	cv::imwrite("LLG_Terfenol_" + std::to_string(int(time1)) + "_u_2d.png", chann);
 
 	for(i=0;i<=ndm;i++){
 		for(j=0;j<=ndm;j++){
